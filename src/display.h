9707c0326362249a5807540420bd54f86216b339
Cosme Charlier <sharkoz@gmail.com>
2026-07-13 10:19:01 +0200

    Add display functions for IP, errors, and clear

--- src/display.h
+++ src/display.h
@@ -4,3 +4,6 @@ void displayInit();
 void displaySetBrightness(int percent);
 void displayShowConnecting();
 void displayShowSetup();
+void displayShowIP(const String& ip);
+void displayShowError(const String& errorMsg);
+void displayClear();
